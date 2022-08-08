ab80af83b69c0be8c1d52e8ef64d166b67a225f5
Sivakoti Sai Deekshitha <Sivakoti.Deekshitha@ivycomptech.com>
2022-08-08 21:30:41 +0530

    comit4

---
+++ Assignmentques2/src/Assignques2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Assignques2 {
+    public static void main(String[] args)
+
+    {
+
+        Scanner s=new Scanner(System.in);
+        int x=s.nextInt();
+        int y=s.nextInt();
+        int z=s.nextInt();
+        if(z==10||z==11||z==16||z==17||z==18||z==19||z==20||z==5||z==4||z==2||z==3)
+
+        {
+
+            System.out.println("Yes");
+        }
+
+        else
+        {
+
+            System.out.println("No");
+        }
+
+    }
+
+
+
+
+}
